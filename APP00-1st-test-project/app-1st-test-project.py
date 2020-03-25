9fefaa5edce1603ac0dfaf2999f39daf0612bc6c
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2020-03-25 19:30:07 +0200

    updated

--- APP00-1st-test-project/app-1st-test-project.py
+++ APP00-1st-test-project/app-1st-test-project.py
@@ -1,3 +1,32 @@
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## USAGE FOR PYTHON
+## Print this help as >> this_script_name --help
+## CREATING SCRIPT USAGE FUNCION AND CALLING IT VIA '--help'
+import sys
+####
+def usage():
+    print('## USAGE: ' + sys.argv[0])
+    HELP_TEXT = """
+    ##############################################################################
+    This program calculates the percentage of marks based upon the individual score
+    for a given student.
+    ##############################################################################
+    """
+    print(HELP_TEXT)
+####
+## Calling the usage function
+## First checking if there are more than one argument on CLI .
+print()
+if (len(sys.argv) > 1) and (sys.argv[1] == "--help"):
+    print('## USAGE HELP IS PRINTED BELOW. SCRIPT WILL EXIT AFTER THAT.')
+    usage()
+    ## EXITING IF ONLY USAGE IS NEEDED
+    quit()
+else:
+    print('## USAGE HELP IS PRINTED BELOW. NORMAL PROGRAM RUN WILL CONTINE AFTER THAT.')
+    usage()  # Printing normal help and continuing script run.
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 whatisthisAbout = """This program calculates the percentage of marks
 based upon the individual score
 for a given student."""
